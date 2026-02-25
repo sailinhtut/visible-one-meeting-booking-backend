51e2c0f97b9ff9c5e2f9a327b642fc58f2382e20
sailinhtut <sailinhtut76062@gmail.com>
2026-02-25 15:44:49 +0700

    Update cors policy 2.0

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173', // react local server
-      'https://visible-one-meeting-booking-website-l80d8e2sb.vercel.app', // production frontend
+      'visible-one-meeting-booking-website.vercel.app', // production frontend
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
